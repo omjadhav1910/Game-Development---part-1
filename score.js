14b26847569d842c903c10bdbdded427f07279bb
Om Jadhav <144478519+omjadhav1910@users.noreply.github.com>
2023-11-02 18:19:57 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
-// Iteration 5: Store the player score and display it on the game over screen
+
+let score = localStorage.getItem("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainBoard = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainBoard.onclick = function(){
+    location.href= "./game.html"
+}
